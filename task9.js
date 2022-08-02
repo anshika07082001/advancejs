e67d82d9e05560366d49f06edd4ba807bbe279ca
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-08-02 14:54:15 +0530

    task9

---
+++ task9.js
@@ -0,0 +1,43 @@
+const arrEntry =[];
+function addEntry()
+{
+    var idInput = document.getElementById('idInput').value;
+    var nameInput = document.getElementById('nameInput').value;
+    var salInput = document.getElementById('salInput').value;
+    try
+    {
+        if(idInput == "" && nameInput == "" && salInput =="")
+        {
+            throw "All fields must be filled";
+        }
+        if(nameInput.match(/[0-9]/)){
+            throw "Name should not contain numbers";
+        }
+        arrEntry.push({ID:idInput,Name:nameInput,TotalSalary:salInput});
+        var text = "<table><tr><td>ID</td><td>Name</td><td>Salary</td></tr>"
+        for(i=0;i<arrEntry.length;i++)
+        {
+            text += "<tr><td>"+arrEntry[i].ID+"</td><td>"+arrEntry[i].Name+"</td><td>"
+            +arrEntry[i].TotalSalary+"</td></tr>";   
+        }
+        text += "</table>";
+        document.getElementById('divTable').innerHTML=text;
+        document.getElementById('idInput').value="";
+        document.getElementById('nameInput').value="";
+        document.getElementById('salInput').value="";
+    }
+    catch(e)
+    {
+        document.getElementById('error').innerHTML = e;
+    }
+}
+populate();
+function populate()
+{
+    var text = "<table><tr><td>ID</td><td>Name</td><td>Salary</td></tr>"
+for(i=0;i<arrEntry.length;i++){
+    text += "<tr><td>"+arrEntry[i].ID+"</td><td>"+arrEntry[i].Name+"</td><td>"
+    +arrEntry[i].TotalSalary+"</td></tr>";
+}
+document.getElementById('divTable').innerHTML=text;
+}
